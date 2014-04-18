9372019ac2f547727861a7a52a1100bfdff92ecb
Kenneth Bruce Uy <missingno15@gmail.com>
2014-04-18 16:51:48 -0400

    Added about page

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do 
+	@dbc = "Hello world!"
 	erb :home
+end
+
+get '/about' do
+	erb :about
+end
+
+not_found do
+  erb :not_found
 end
